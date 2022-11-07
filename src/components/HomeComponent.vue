835fd2c55ceb7645ccf86ef1114bbb072a54dfcf
NERFOO <james.jaime.1105@gmail.com>
2022-11-07 14:27:05 +0100

    service

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,15 +1,28 @@
 <template>
     <div>
         <h1>Home component</h1>
+        <h2 style="color:purple">{{saludo}}</h2>
         <img src="./../assets/images/img1.jpg" style="width:100%"/>
     </div>
 </template>
 
 <script>
-export default {
-    name : "HomeComponent" ,
+    import ServiceEjemplo from './../services/ServiceEjemplo';
+    //PARA PODER UTILIZAR NUESTRO SERVICIO DEBEMOS INSTANCIAR UN OBJETO DE LA CLASE EN EL COMPONENT
+    const service = new ServiceEjemplo();
 
-}
+
+    export default {
+        name : "HomeComponent" ,
+        data() {
+            return{
+                saludo : ""
+            }
+        },
+        mounted() {
+            this.saludo = service.getSaludo("NERFO");
+        }
+    }
 </script>
 
 <style>
